18ce245f207811d8d8fbc9766ef7f5cec1a5af83
Phil Chuang <dev@philchuang.com>
2014-03-13 00:00:29 -0400

    added listings sort, min price filter

--- src/app/js/controllers/ListingsController.js
+++ src/app/js/controllers/ListingsController.js
@@ -1,10 +1,27 @@
 "use strict";
 
+var ListingsControllerBase = ListingsControllerBase || {};
+
+ListingsControllerBase.prepareListing = function (listing) {
+    if (typeof listing === "undefined" || listing === null) return;
+    listing.streetNameAndNumber = listing.streetName + " " + listing.streetNumber;
+    listing.lastUpdate = Math.max.apply (Math, listing.history.map (function (h) { return h.timestamp; }));
+};
+
+ListingsControllerBase.prepareListings = function (listings) {
+    if (typeof listings === "undefined" || listings === null) return;
+    for (var i = 0; i < listings.length; i++) {
+        ListingsControllerBase.prepareListing (listings[i]);
+    }
+};
+
 app.controller ("ListingsController",
     function ($scope, storageService) {
 
         var refresh = function () {
             storageService.getAllListings().then(function (listings) {
+                ListingsControllerBase.prepareListings(listings);
+                $scope.listingsSortAsc = true;
                 $scope.listings = listings;
             });
         };
