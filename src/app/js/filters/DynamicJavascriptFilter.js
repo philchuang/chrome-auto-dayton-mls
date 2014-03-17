6286b33ada0ff23e18b065727313650d86fab8c2
Phil Chuang <dev@philchuang.com>
2014-03-17 17:06:13 -0400

    fixed custom filter

--- src/app/js/filters/DynamicJavascriptFilter.js
+++ src/app/js/filters/DynamicJavascriptFilter.js
@@ -16,23 +16,44 @@ app.filter ("dynamicJavascript", function () {
         /* custom filter to see if a listing has a study
 
         if (scope == null || angular.isUndefined (scope.rooms) || scope.rooms.length === 0) return false;
-        return scope.rooms.filter (function (r) { return r.name.match (new RegExp ("study", "i")) != null; }) != null;
+        return scope.rooms.filter (function (r) { return r.name.match (new RegExp ("study", "i")) != null; }).length > 0;
+
+         */
+
+        /* custom filter to see if a listing has changed in price
+
+        if (scope == null || angular.isUndefined (scope.history) || scope.history.length === 0) return false;
+        return scope.history.filter (function (h) { return h.action.match (new RegExp ("listPrice:", "i")) != null; }).length > 0;
 
          */
 
         var predicate = function (scope) { return true; };
         try {
-            predicate = new Function("scope", dynamicJs);
+            predicate = new Function ("scope", dynamicJs);
             predicate (null); // test to see if it crashes outright
+        } catch (ex) {
+            console.log ("error in dynamic javascript: " + ex.message);
+            return filtered;
         }
-        catch (ex) { return filtered; }
 
         filtered.splice (0, filtered.length);
 
-        for (var i2 = 0; i2 < array.length; i2++) {
+        var printedError = false;
+        for (var i2 = 0; i2 < array.length; i2++)
+        {
             var item = array[i2];
-            try { if (predicate(item)) filtered.push(item); }
-            catch (ex) { continue; }
+            try
+            {
+                if (predicate (item)) filtered.push (item);
+            }
+            catch (ex)
+            {
+                if (printedError === false) {
+                    console.log("error in dynamic javascript: " + ex.message);
+                    printedError = true;
+                }
+                continue;
+            }
         }
 
         return filtered;
