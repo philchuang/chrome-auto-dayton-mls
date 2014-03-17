1d0a5191de0bc082bbfc6c8f3edc3a54c178f6df
Phil Chuang <dev@philchuang.com>
2014-03-16 22:53:34 -0400

    fixed filter field control

--- src/app/js/directives/ClearControl.js
+++ src/app/js/directives/ClearControl.js
@@ -1,17 +1,21 @@
 ﻿"use strict";
 
 app.directive ("clearControl", function () {
+    // this is not a best practice
+    // look into http://plnkr.co/edit/KqMIAC6MKPAVrA6LUY87?p=preview
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
-            var input = element.prev().first();
+            var input = element.prev ().first ();
             if (element[0].tagName === "A" || element[0].tagName === "a")
                 element[0].href = "#"; // I hate this, prevent it from showing up in URL!
             element.bind ("click", function (e) {
                 e.preventDefault = true;
-                input.scope().$apply (function (s) {
-                    input.val (""); // DAMMIT WHY DOESN'T THIS UPDATE FILTERING
-                });
+                input.val ("");
+                if (input[0].nodeName === "SELECT")
+                    input.trigger ("change");
+                else
+                    input.trigger ("input");
             });
         }
     };
