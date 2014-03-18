6bb3d27115743d3e36f710b964fd9a2807b097f4
Phil Chuang <dev@philchuang.com>
2014-03-18 16:27:33 -0400

    debounced filter inputs

---
+++ src/app/js/directives/UsDebounceDirective.js
@@ -0,0 +1,38 @@
+﻿"use strict";
+
+// underscore debounce
+app.directive('usDebounce', function ($timeout) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        priority: 99,
+        link: function (scope, element, attr, ngModelCtrl) {
+
+            if (attr.type === 'radio' || attr.type === 'checkbox') {
+                return;
+            }
+
+            var delayMs = parseInt (attr.usDebounce, 10);
+            if (isNaN (delayMs)) {
+                delayMs = 500;
+            }
+
+            element.unbind ("input");
+
+            var debounce;
+            element.bind ("input", function () {
+                $timeout.cancel (debounce);
+                debounce = $timeout (function () {
+                    scope.$apply (function () {
+                        ngModelCtrl.$setViewValue (element.val());
+                    });
+                }, delayMs);
+            });
+            element.bind ('blur', function () {
+                scope.$apply (function () {
+                    ngModelCtrl.$setViewValue (element.val ());
+                });
+            });
+        }
+    };
+});
