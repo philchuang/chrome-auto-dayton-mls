b00190b1377310250c73bfbaed3d488e261a402e
Phil Chuang <dev@philchuang.com>
2014-03-25 09:58:46 -0400

    cleanup

--- crx/prepare_for_pack.bat
+++ crx/prepare_for_pack.bat
@@ -3,6 +3,7 @@ echo This will delete files that aren't necessary for the chrome extension!
 echo Please ensure all files are committed to source control first!
 echo Press Ctrl-C to cancel
 pause
+
 echo.
 echo Deleting unnecessary files...
 del ..\src\*.csproj
@@ -13,6 +14,12 @@ del ..\src\Web.*.config
 rd  ..\src\Properties /s /q
 rd  ..\src\bin /s /q
 rd  ..\src\obj /s /q
+del ..\src\lib\bootstrap\css\bootstrap-theme.css
+del ..\src\lib\bootstrap\css\bootstrap-theme.css.map
+del ..\src\lib\bootstrap\css\bootstrap.css
+del ..\src\lib\bootstrap\css\bootstrap.css.map
+del ..\src\lib\bootstrap\js\bootstrap.js
+
 echo.
 echo Files deleted, chrome extension is ready to pack.
 echo After packing, remember to only check in the crx, and to restore the deleted files!
